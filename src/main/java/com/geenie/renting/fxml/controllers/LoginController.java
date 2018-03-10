ba253d85db3b6e5c6013c3a47e0e3b138c7836c5
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-03-10 18:47:13 +0100

    Login Pane + Hotel Pane + Appartment Pane + New User Pane skeleton
    Controller

--- src/main/java/com/geenie/renting/fxml/controllers/LoginController.java
+++ src/main/java/com/geenie/renting/fxml/controllers/LoginController.java
@@ -10,6 +10,13 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
 
+import com.geenie.renting.fxml.utils.Utils;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -18,11 +25,73 @@ import javafx.fxml.Initializable;
  */
 public class LoginController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
+	private final static String gotoLoginPane = "loginPane";
+
+	@FXML
+	private AnchorPane parentPane;
+
+	@FXML
+	private AnchorPane loginPane;
+	@FXML
+	private AnchorPane hotelPane;
+	@FXML
+	private AnchorPane appartmentPane;
+
+	@FXML
+	private JFXTextField usernameLoginTF;
+
+	@FXML
+	private JFXTextField passwordLoginTF;
+
+	@FXML
+	private JFXButton logoutHotelPane;
+
+	@FXML
+	private JFXButton logoutAppartmentPane;
+	@FXML
+	private AnchorPane newUserPane;
+
+	@FXML
+	private JFXButton logoutNewUserPane;
+
+	/**
+	 * Initializes the controller class.
+	 */
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Utils.switchPanes(gotoLoginPane, parentPane);
+		// TODO
+	}
+
+	@FXML
+	void unlockAccess(ActionEvent event) {
+		// TODO
+		String gotos = usernameLoginTF.getText();
+		if (gotos.equals("hafa")) {
+			Utils.switchPanes("appartmentPane", parentPane);
+		}
+		if (gotos.equals("aymen")) {
+			Utils.switchPanes("hotelPane", parentPane);
+		}
+		else{
+			Utils.switchPanes("newUserPane", parentPane);
+		}
+	}
+
+	@FXML
+	void logoutAppartment(ActionEvent event) {
+		Utils.switchPanes(gotoLoginPane, parentPane);
+
+	}
+
+	@FXML
+	void logoutHotel(ActionEvent event) {
+		Utils.switchPanes(gotoLoginPane, parentPane);
+	}
+
+	@FXML
+	void logoutNewUser(ActionEvent event) {
+		Utils.switchPanes(gotoLoginPane, parentPane);
+	}
+
 }
