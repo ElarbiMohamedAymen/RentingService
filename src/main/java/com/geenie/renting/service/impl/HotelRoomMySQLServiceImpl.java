5df747425626fa6fca383b9e53066e230f13663a
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-03-18 11:26:31 +0100

    Adding hotel room method

--- src/main/java/com/geenie/renting/service/impl/HotelRoomMySQLServiceImpl.java
+++ src/main/java/com/geenie/renting/service/impl/HotelRoomMySQLServiceImpl.java
@@ -1,7 +1,20 @@
 package com.geenie.renting.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.geenie.renting.beans.HotelRoom;
+import com.geenie.renting.repository.HotelRoomRepository;
 import com.geenie.renting.service.interfaces.IHotelRoomMySQLService;
 
-public class HotelRoomMySQLService implements IHotelRoomMySQLService {
+public class HotelRoomMySQLServiceImpl implements IHotelRoomMySQLService {
+
+	@Autowired
+	private HotelRoomRepository hotelRoomRepository;
+	
+	@Override
+	public void addHotelRoom(HotelRoom hotelRoom) {
+		hotelRoomRepository.save(hotelRoom);
+		
+	}
 
 }
--- src/main/java/com/geenie/renting/service/interfaces/IHotelRoomMySQLService.java
+++ src/main/java/com/geenie/renting/service/interfaces/IHotelRoomMySQLService.java
@@ -1,5 +1,9 @@
 package com.geenie.renting.service.interfaces;
 
+import com.geenie.renting.beans.HotelRoom;
+
 public interface IHotelRoomMySQLService {
+	
+	public void addHotelRoom(HotelRoom hotelRoom);
 
 }
