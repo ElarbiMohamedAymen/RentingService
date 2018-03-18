137b79d7d0f63875dc4430522d8b63bd260e587f
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-03-18 11:26:59 +0100

    Add Stay method

--- src/main/java/com/geenie/renting/service/impl/StayMySQLServiceImpl.java
+++ src/main/java/com/geenie/renting/service/impl/StayMySQLServiceImpl.java
@@ -1,7 +1,19 @@
 package com.geenie.renting.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.geenie.renting.beans.Stay;
+import com.geenie.renting.repository.StayRepository;
 import com.geenie.renting.service.interfaces.IStayMySQLService;
 
-public class StayMySQLService implements IStayMySQLService {
+public class StayMySQLServiceImpl implements IStayMySQLService {
+
+	@Autowired
+	private StayRepository stayRepository;
+	
+	@Override
+	public void addStay(Stay stay) {
+		stayRepository.save(stay);
+	}
 
 }
--- src/main/java/com/geenie/renting/service/interfaces/IStayMySQLService.java
+++ src/main/java/com/geenie/renting/service/interfaces/IStayMySQLService.java
@@ -1,5 +1,8 @@
 package com.geenie.renting.service.interfaces;
 
+import com.geenie.renting.beans.Stay;
+
 public interface IStayMySQLService {
 
+	public void addStay(Stay stay);
 }
