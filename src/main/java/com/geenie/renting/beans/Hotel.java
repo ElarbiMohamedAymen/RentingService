2ffb6a7dd22a563cf9cfe420bdebef5fe1342e36
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-03-18 11:44:21 +0100

    FIXING data too long for user attribut

--- src/main/java/com/geenie/renting/beans/Hotel.java
+++ src/main/java/com/geenie/renting/beans/Hotel.java
@@ -9,6 +9,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 
 @Entity
@@ -24,6 +25,7 @@ public class Hotel implements Serializable {
 	private int roomNumber;
 	private String address;
 	private String hotelName;
+	@Lob
 	private User manager;
 	@OneToMany(mappedBy = "hotel", cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
 	private List<HotelRoom> rooms;
