3990b2f8d51147a55c1b268f88cfadf131e97712
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-03-30 21:57:57 +0100

    Adding nouserfoundException

---
+++ src/main/java/com/geenie/renting/exceptions/NoUserFoundException.java
@@ -0,0 +1,13 @@
+package com.geenie.renting.exceptions;
+
+public class NoUserFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoUserFoundException(String message) {
+		super(message);
+	}
+}
