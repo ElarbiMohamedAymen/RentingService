5851e095e3dbcdda6c5e9dcdf39a84640f0a51ae
ElarbiMohamedAymen <mohamedaymen.elarbi@esprit.tn>
2018-03-30 21:53:39 +0100

    Removing CascadeType to avoid, detached entity

--- src/main/java/com/geenie/renting/beans/Hotel.java
+++ src/main/java/com/geenie/renting/beans/Hotel.java
@@ -3,13 +3,11 @@ package com.geenie.renting.beans;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
